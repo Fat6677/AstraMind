3a7c76278573804cd3e09c88cc49137ca5bef568
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2025-11-20 02:32:02 +0700

    melakukan perubahan tampilan

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -9,15 +9,11 @@ export default function HeaderWithLogo() {
       {/* HEADER */}
       <header className="flex justify-between items-center px-6 py-4 bg-blue-600 text-white">
         <h1 className="text-xl font-bold">astro-ai</h1>
-
         <Link
           href="/login"
           className="bg-blue-600 text-white px-4 py-2 rounded-lg font-semibold hover:bg-white hover:text-blue-700 transition shadow"
-        >
-          Login
+        > Login
         </Link>
-
-
       </header>
 
       {/* TEKS SELAMAT DATANG */}
@@ -32,27 +28,25 @@ export default function HeaderWithLogo() {
       </h2>
 
       {/* LOGO + NAMA */}
-      <div className="flex flex-col  ml-10">
+      <div className="flex items-start gap-10 px-10 mt-10 ml-40">
         {/* LOGO */}
-        <div className="w-32 h-32 rounded-full flex items-center justify-center shadow-lg overflow-hidden">
+        <div className="w-70 h-70 rounded-full flex items-center justify-center shadow-lg overflow-hidden">
           <img src="logo.png" alt="logo" />
         </div>
 
-        {/* NAMA */}
-        <h2 className="text-2xl font-semibold mt-4 text-gray-800">
-          Astro-Ai
-        </h2>
-       
+        
+         {/* DESKRIPSI */}
+      <p className="mt-6 max-w-xl text-gray-600 px-6 leading-relaxed text-justify">
+        Astro AI adalah sebuah kecerdasan buatan yang dirancang sebagai pendamping cerdas dalam kehidupan sehari-hari. Dengan kemampuan memahami konteks percakapan, menganalisis kebutuhan pengguna, serta memberikan rekomendasi yang tepat, Astro AI hadir sebagai “AI Permal Kehidupan” — sebuah asisten digital yang selalu siap membantu berbagai aktivitas harian.
+
+        Astro AI mampu menjawab pertanyaan, memberikan solusi praktis, membantu pengambilan keputusan, serta mendampingi pengguna dalam belajar, bekerja, maupun berkreasi. Dengan antarmuka yang ramah dan respons cepat, Astro AI memberikan pengalaman interaksi yang intuitif dan personal, seperti berbicara dengan seorang asisten nyata.
+
+        Dibangun dengan teknologi modern dan kecerdasan adaptif, Astro AI terus berkembang mengikuti kebiasaan dan kebutuhan penggunanya. Mulai dari mencari informasi, merencanakan kegiatan, membuat konten, hingga membantu pekerjaan teknis, Astro AI menjadi partner cerdas yang setia di setiap momen penting dalam kehidupan.
+      </p>
+
 
       </div>
-       {/* DESKRIPSI */}
-        <p className="mt-6 max-w-xl text-gray-600 px-6 leading-relaxed text-justfy">
-          Astro AI adalah sebuah kecerdasan buatan yang dirancang sebagai pendamping cerdas dalam kehidupan sehari-hari. Dengan kemampuan memahami konteks percakapan, menganalisis kebutuhan pengguna, serta memberikan rekomendasi yang tepat, Astro AI hadir sebagai “AI Permal Kehidupan” — sebuah asisten digital yang selalu siap membantu berbagai aktivitas harian.
-
-          Astro AI mampu menjawab pertanyaan, memberikan solusi praktis, membantu pengambilan keputusan, serta mendampingi pengguna dalam belajar, bekerja, maupun berkreasi. Dengan antarmuka yang ramah dan respons cepat, Astro AI memberikan pengalaman interaksi yang intuitif dan personal, seperti berbicara dengan seorang asisten nyata.
-
-          Dibangun dengan teknologi modern dan kecerdasan adaptif, Astro AI terus berkembang mengikuti kebiasaan dan kebutuhan penggunanya. Mulai dari mencari informasi, merencanakan kegiatan, membuat konten, hingga membantu pekerjaan teknis, Astro AI menjadi partner cerdas yang setia di setiap momen penting dalam kehidupan.
-        </p>
+     
     </div>
   );
 }
